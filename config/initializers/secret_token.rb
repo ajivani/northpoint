41b906cc9edab1f17be90813daad643a6ce46ba3
Ameen Jivani <ajivani@gmail.com>
2010-10-14 22:21:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Northpoint::Application.config.secret_token = '4eacb6d6f8617c89fcf9c02619df7ca040012f1c5a7375bf40205cd713a2998b7365e8d7ac1abe1c3cd9a28024aa0ac178f7ac2ae164f161202e587a78c3374e'
