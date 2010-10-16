11b7ed3835c45326623d36f67cd66b522bf716f6
Ameen Jivani <ajivani@gmail.com>
2010-10-15 21:45:48 -0700

    added a footer and a home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def logo
-    logo = image_tag("rails.png", :alt=>"NorthPoint Canadian", :class=>"round")
+    logo = image_tag("logo.png", :alt=>"NorthPoint Canadian", :class=>"round")
   end
 
 end
