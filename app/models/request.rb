4bb0c85dc6d7839fc575f385f800116fba567e00
Ameen Jivani <ajivani@gmail.com>
2010-11-08 01:52:15 -0800

    fixed up the vadiation for the email request_form eamil@something.com is valid now

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,5 @@
 class Request < ActiveRecord::Base
-    validates :email, :company_name, :customer_name, :phone_number, :quote_details, :presence=>true
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :company_name, :customer_name, :phone_number, :quote_details, :presence=>true
+  validates :email, :presence=>true, :format => {:with=> email_regex}
 end
