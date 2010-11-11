372929eaf382851b5f7526b3b69cc11b8bc4131e
Ameen Jivani <ajivani@gmail.com>
2010-11-10 18:55:09 -0800

    order by sku

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,6 @@ class Product < ActiveRecord::Base
   validates :sku, :presence=>true
   validates :image_url, :presence=>true
   #added so that it's ordered in
-  default_scope :order => 'products.name'
+  default_scope :order => 'products.sku DESC'
 
 end
